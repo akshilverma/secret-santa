5c3f430b6ec7b4c6b9e8b6d3a55b5018a0ab904f
akshilverma <verma.akshil@gmail.com>
2025-01-07 01:26:36 +0530

    Add eslint and prettier configurations

---
+++ secret-santa-service/eslint.config.mjs
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ["src/**/*.{js,mjs,cjs,ts}", "test/**/*.{js,mjs,cjs,ts}"] },
+	{ languageOptions: { globals: globals.node } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	{
+		rules: {
+			complexity: ["error", 5],
+		},
+	},
+	{ extends: ["prettier"] },
+];
