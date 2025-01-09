fe4b2a4e8d59d8a4ef6585b3b727f56246c39c5b
akshilverma <verma.akshil@gmail.com>
2025-01-10 01:10:52 +0530

    - Remove nodemon - Add pre-commit hook using husky - Add support for .env files

--- secret-santa-service/src/server.ts
+++ secret-santa-service/src/server.ts
@@ -2,14 +2,14 @@ import express from "express";
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = "3000" } = process.env;
 
 // TODO: convert app to application.ts and add routes
 app.get("/health", (_, res) => {
 	res.status(200).json({ status: "ok" });
 });
 
-app.get("/api/v1", () => console.log("API v1"));
+app.get("/api/v1", () => { console.log("API v1"); });
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
