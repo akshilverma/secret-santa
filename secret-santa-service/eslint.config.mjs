04ac640fc54c55ba859abd61214601f5bedf9a1b
akshilverma <verma.akshil@gmail.com>
2025-01-10 01:09:04 +0530

    Fix eslint flat config not working

--- secret-santa-service/eslint.config.mjs
+++ secret-santa-service/eslint.config.mjs
@@ -1,17 +1,45 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	{ files: ["src/**/*.{js,mjs,cjs,ts}", "test/**/*.{js,mjs,cjs,ts}"] },
-	{ languageOptions: { globals: globals.node } },
-	pluginJs.configs.recommended,
-	...tseslint.configs.strictTypeChecked,
 	{
-		rules: {
-			complexity: ["error", 5],
+		files: ["src/**/*.{js,mjs,cjs,ts}", "test/**/*.{js,mjs,cjs,ts}"],
+		languageOptions: {
+			globals: globals.node,
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
 	},
-	{ extends: ["prettier"] },
+	pluginJs.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	eslintConfigPrettier,
+	{
+		rules: {
+			semi: ["warn", "always"],
+			eqeqeq: "warn",
+			"no-unused-vars": "error",
+			"no-trailing-spaces": "warn",
+			"eol-last": [
+				"error",
+				"always"
+			],
+			"no-multiple-empty-lines": [
+				"error",
+				{
+					"max": 2,
+					"maxEOF": 1
+				}
+			],
+			"complexity": [
+				"error",
+				5
+			]
+		},
+	}
 ];
