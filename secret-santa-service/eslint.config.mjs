3983e9fa44ce858cba50b74b633d2839e6cf5b7a
akshilverma <verma.akshil@gmail.com>
2025-01-22 16:43:51 +0530

    Move tsconfig and eslint back to individual service folders - Add tsconfig paths

---
+++ secret-santa-service/eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{
+		ignores: ["node_modules/", "dist/", "collections/", "eslint.config.mjs"],
+	},
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		languageOptions: {
+			globals: globals.node,
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	pluginJs.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	eslintConfigPrettier,
+	{
+		rules: {
+			semi: ["warn", "always"],
+			eqeqeq: "warn",
+			"no-unused-vars": "error",
+			"no-trailing-spaces": "warn",
+			"eol-last": ["error", "always"],
+			"no-multiple-empty-lines": [
+				"error",
+				{
+					max: 2,
+					maxEOF: 1,
+				},
+			],
+			complexity: ["error", 5],
+		},
+	},
+];
