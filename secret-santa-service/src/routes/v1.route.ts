41d26c4bbe1b9b24e24d81f58ab0cbc7895eefc5
akshilverma <verma.akshil@gmail.com>
2025-03-04 01:00:46 +0530

    Enable CORS for register route

--- secret-santa-service/src/routes/v1.route.ts
+++ secret-santa-service/src/routes/v1.route.ts
@@ -1,13 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import cors, { CorsOptions } from "cors";
 import { Router, json } from "express";
 
 import { registerController } from "@/controllers/v1.controller";
 
 const v1Router = Router();
 
+const corsOptions: CorsOptions = {
+    origin: "http://localhost:5173",
+    methods: "POST",
+};
+
+// Enable CORS for register route
+v1Router.use("/register", cors(corsOptions));
+
 v1Router
-    .post("/register", json(), registerController)
-    // Testing only. To be deleted.
+    .post("/register", cors(corsOptions), json(), registerController)
+    // TODO: Testing only. To be deleted.
     .get("/register", async (req, res) => {
         const { userId } = req.query;
         const prisma = new PrismaClient();
